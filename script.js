f9f6c75a8c25a0d26a6286db5b969a15b10da724
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2024-01-04 14:02:29 +0100

    updated functionallity

--- script.js
+++ script.js
@@ -4,9 +4,23 @@ let inputLjiljani = document.getElementById("ljiljani");
 let inputGerberi = document.getElementById("gerberi");
 let divSlika = document.querySelector(".slikovni-Prikaz");
 
+// let chbxBombonjera = document.querySelector('.chbx-bombonjera');
+// let chbxCokoloda = document.querySelector('.chbx-cokoloada');
+// let chbxSampanjac = document.querySelector('.chbx-sampanjac');
+
+let checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+let checked = [];
+checkboxes.forEach((checkbox) => {
+  checked.push(checkbox.value);
+});
+
 btnIzracunaj.addEventListener("click", function (event) {
   event.preventDefault();
   divSlika.innerHTML = "";
+
+  let naslov = document.createElement("h1");
+  divSlika.appendChild(naslov);
+  naslov.textContent = "Vas buket izgleda ovako:";
 
   let ruzeKom = parseInt(inputRuze.value) || 0;
   let ljiljaniKom = parseInt(inputLjiljani.value) || 0;
@@ -19,8 +33,13 @@ btnIzracunaj.addEventListener("click", function (event) {
   function dodajSliku(src, klasa) {
     let novaSlika = document.createElement("img");
     novaSlika.src = src;
-    divSlika.appendChild(novaSlika);
     novaSlika.classList.add(klasa);
+    divSlika.appendChild(novaSlika);
+  }
+  function dodajParagraf(textContent) {
+    let noviP = document.createElement("p");
+    divSlika.appendChild(noviP);
+    noviP.textContent = textContent;
   }
 
   for (let i = 0; i < ruzeKom; i++) {
@@ -33,6 +52,21 @@ btnIzracunaj.addEventListener("click", function (event) {
 
   for (let i = 0; i < gerberiKom; i++) {
     dodajSliku(srcSlikeG, "gerberiAvatar");
+  }
+
+  for (let i = 0; i < checked.length; i++) {
+    if (checked[i].value == Bombonjera) {
+      texContent = `+ ${checked[i].value}`;
+      dodajParagraf(texContent);
+    }
+    if (checked[i].value == Cokolada) {
+      texContent = `+ ${checked[i].value}`;
+      dodajParagraf(texContent);
+    }
+    if (checked[i].value == Sampanjac) {
+      texContent = `+ ${checked[i].value}`;
+      dodajParagraf(texContent);
+    }
   }
 });
 
